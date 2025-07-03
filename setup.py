118ec253a53948f7b0f3ed381bb998cac3986a2d
Karen Leung <karen.ym.leung@gmail.com>
2025-07-02 18:29:27 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="karenl7testci",
+    version="0.0.1",
+    description="testing continuous integration",
+    author="Karen Leung",
+    author_email="karen.ym.leung@gmail.com",
+    packages=["karenl7testci"],
+    package_dir={"karenl7testci": "src/karenl7-test-ci"},
+    install_requires=[
+        "numpy",
+    ],
+)
